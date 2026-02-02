4c7610493d9b468284e6231300a9cbf5c6601779
Internship <keerthanar.1804@gmail.com>
2026-02-02 19:20:15 +0530

    db

--- backend/db.py
+++ backend/db.py
@@ -2,10 +2,4 @@ import psycopg2
 import os
 
 def get_db():
-    return psycopg2.connect(
-        host="localhost",
-        database="donation",
-        user="postgres",
-        password="1234",
-        port=5432
-    )
+    return psycopg2.connect(os.environ["DATABASE_URL"])
